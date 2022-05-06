a7b3cae25ad3ee2b436a5a22cc7108b4770cef29
Vishnudas <73219501+vishnudas-bluefox@users.noreply.github.com>
2022-05-06 15:18:49 +0000

    Menu driven for multi level scrapping

--- test.rb
+++ test.rb
@@ -1,26 +1,4 @@
-
-require 'nokogiri'
-require 'open-uri'
-
-# Fetch and parse HTML document
-doc = Nokogiri::HTML(URI.open('https://www.freethink.com/articles'))
-
-# Search for nodes by css
-puts "css\n ==============================="
-doc.css('nav ul.menu li a', 'article h2').each do |link|
-
-
-  puts link.content
-end
-
-# Search for nodes by xpath
-puts "xpath \n====================================="
-doc.xpath('//nav//ul//li/a', '//article//h2').each do |link|
-  
-  puts link.content
-end
-
-# Or mix and match
-doc.search('nav ul.menu li a', '//article//h2').each do |link|
-  puts link.content
+n=10
+n.times do |index|
+  puts index
 end
